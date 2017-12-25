9ad73a981813248e3bc2a68b19ce7473094c2492
hetao <hetao@hetaodeMacBook-Pro.local>
2017-12-25 17:38:36 +0800

    生命周期

--- Contacts/src/App.js
+++ Contacts/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactAPI from './utils/ContactsAPI'
 
 // const contacts = [
 //   {
@@ -24,35 +25,75 @@ import ListContacts from './ListContacts'
 class App extends Component {
   state = {
     contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
+      // {
+      //   "id": "ryan",
+      //   "name": "Ryan Florence",
+      //   "email": "ryan@reacttraining.com",
+      //   "avatarURL": "http://localhost:5001/ryan.jpg"
+      // },
+      // {
+      //   "id": "michael",
+      //   "name": "Michael Jackson",
+      //   "email": "michael@reacttraining.com",
+      //   "avatarURL": "http://localhost:5001/michael.jpg"
+      // },
+      // {
+      //   "id": "tyler",
+      //   "name": "Tyler McGinnis",
+      //   "email": "tyler@reacttraining.com",
+      //   "avatarURL": "http://localhost:5001/tyler.jpg"
+      // }
     ]
   }
 
   removeContact = (contact) => {
     // console.log("log contact:", contact);
     // console.log("log contact.id:", contact.id);
-    this.setState((state) => (
-      {
-        contacts: state.contacts.filter((c) => c.id !== contact.id)
-      }
-    ))
+    // this.setState((state) => (
+    //   {
+    //     contacts: state.contacts.filter((c) => c.id !== contact.id)
+    //   }
+    // ))
+
+    ContactAPI.remove(contact).then(
+        (data) => {
+            this.getAll()
+        }
+    )
+  }
+
+  getAll = () => {
+      ContactAPI.getAll().then(
+          (contacts) => {
+              this.setState({contacts})
+          }
+      )
+  }
+
+    componentWillMount(){
+      console.log("componentWillMount");
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+    componentWillReceiveProps() {
+        console.log("componentWillReceiveProps");
+    }
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate");
+        return true;
+    }
+    componentWillUpdate() {
+        console.log("componentWillUpdate");
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
+
+
+  componentDidMount() {
+      console.log("componentDidMount");
+      this.getAll();
   }
 
   render() {
